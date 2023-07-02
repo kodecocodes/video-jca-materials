d617164f69dcf99eb90daf877143e35d2d71b939
Harun Wangereka <harunwangereka@gmail.com>
2023-07-02 23:00:01 +0200

    changes

---
+++ 05-fading-between-two-composables-with-crossfade/starter/app/src/main/java/com/kodeco/android/opinionator/theme/Colors.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2023 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.kodeco.android.opinionator.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF7743B5)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFEEDBFF)
+val md_theme_light_onPrimaryContainer = Color(0xFF2A0053)
+val md_theme_light_secondary = Color(0xFF6D23F9)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFE8DDFF)
+val md_theme_light_onSecondaryContainer = Color(0xFF22005D)
+val md_theme_light_tertiary = Color(0xFF805159)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFD9DE)
+val md_theme_light_onTertiaryContainer = Color(0xFF321018)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF1D1B1E)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF1D1B1E)
+val md_theme_light_surfaceVariant = Color(0xFFE8E0EB)
+val md_theme_light_onSurfaceVariant = Color(0xFF4A454E)
+val md_theme_light_outline = Color(0xFF7B757F)
+val md_theme_light_inverseOnSurface = Color(0xFFF5EFF4)
+val md_theme_light_inverseSurface = Color(0xFF322F33)
+val md_theme_light_inversePrimary = Color(0xFFDAB9FF)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF7743B5)
+val md_theme_light_outlineVariant = Color(0xFFCCC4CF)
+val md_theme_light_scrim = Color(0xFF000000)
+
+val md_theme_dark_primary = Color(0xFFDAB9FF)
+val md_theme_dark_onPrimary = Color(0xFF460283)
+val md_theme_dark_primaryContainer = Color(0xFF5E289B)
+val md_theme_dark_onPrimaryContainer = Color(0xFFEEDBFF)
+val md_theme_dark_secondary = Color(0xFFCFBDFF)
+val md_theme_dark_onSecondary = Color(0xFF3A0093)
+val md_theme_dark_secondaryContainer = Color(0xFF5300CD)
+val md_theme_dark_onSecondaryContainer = Color(0xFFE8DDFF)
+val md_theme_dark_tertiary = Color(0xFFF2B7C0)
+val md_theme_dark_onTertiary = Color(0xFF4B252C)
+val md_theme_dark_tertiaryContainer = Color(0xFF653B42)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFD9DE)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF1D1B1E)
+val md_theme_dark_onBackground = Color(0xFFE7E1E5)
+val md_theme_dark_surface = Color(0xFF1D1B1E)
+val md_theme_dark_onSurface = Color(0xFFE7E1E5)
+val md_theme_dark_surfaceVariant = Color(0xFF4A454E)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCCC4CF)
+val md_theme_dark_outline = Color(0xFF958E98)
+val md_theme_dark_inverseOnSurface = Color(0xFF1D1B1E)
+val md_theme_dark_inverseSurface = Color(0xFFE7E1E5)
+val md_theme_dark_inversePrimary = Color(0xFF7743B5)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFDAB9FF)
+val md_theme_dark_outlineVariant = Color(0xFF4A454E)
+val md_theme_dark_scrim = Color(0xFF000000)
+
+
+val seed = Color(0xFFBB86FC)
+val darkTransparent = Color(0f, 0f, 0f, 0.5f)
