1f421137491aa9cf2c3085235a55ad36b8b5a446
Harun Wangereka <harunwangereka@gmail.com>
2023-07-09 10:40:27 +0400

    starter project ready ep 7

---
+++ 07-introduction-to-updatetransition/final/app/src/main/java/com/kodeco/android/opinionator/MainActivity.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2023 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ * 
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.kodeco.android.opinionator
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.kodeco.android.opinionator.feed.FeedScreen
+import com.kodeco.android.opinionator.theme.OpinionatorTheme
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      var boxState = remember { mutableStateOf(BoxState.Shrunk) }
+      val transition = updateTransition(boxState, "Box Transition")
+
+      val size = transition.animateDp(label = "Size Transition") { state ->
+        when (state.value) {
+          BoxState.Shrunk -> 100.dp
+          BoxState.Expanded -> 300.dp
+        }
+      }
+
+      val color = transition.animateColor(label = "Color Transition") { state ->
+        when (state.value) {
+          BoxState.Shrunk -> Color.Cyan
+          BoxState.Expanded -> Color.Red
+        }
+      }
+
+      Box(
+          modifier = Modifier
+              .width(size.value)
+              .height(size.value)
+              .background(color.value)
+              .clickable {
+                boxState.value = when(boxState.value)
+                {
+                  BoxState.Shrunk -> BoxState.Expanded
+                  BoxState.Expanded -> BoxState.Shrunk
+                }
+              }
+      )
+    }
+  }
+}
+enum class BoxState {
+  Shrunk,
+  Expanded
+}
