e1a30c2afca9a76d2d366db4c349360fef6fabc5
Harun Wangereka <harunwangereka@gmail.com>
2023-08-05 00:21:39 +0300

    ep 10 final update deps & As version

--- 10-controlling-animation-properties/final/app/src/main/java/com/kodeco/android/opinionator/feed/CommentBar.kt
+++ 10-controlling-animation-properties/final/app/src/main/java/com/kodeco/android/opinionator/feed/CommentBar.kt
@@ -153,8 +153,8 @@ private fun useLikeCountAnimation(likes: Int): LikeCountAnimation {
     }
   }
   val translationPx = with(LocalDensity.current) { translation.toPx() }
-  val alpha by transition.animateFloat(
-    label = "Alpha",
+  val alphaAnimation by transition.animateFloat(
+    label = "Alpha Animation",
     transitionSpec = { spring(stiffness = Spring.StiffnessLow) }
   ) { animationState ->
     when (animationState) {
@@ -164,7 +164,7 @@ private fun useLikeCountAnimation(likes: Int): LikeCountAnimation {
   }
 
   val isFinished = transition.currentState == transition.targetState
-  return LikeCountAnimation(alpha, translationPx, isFinished)
+  return LikeCountAnimation(alphaAnimation, translationPx, isFinished)
 }
 
 private enum class LikeAnimationState {
